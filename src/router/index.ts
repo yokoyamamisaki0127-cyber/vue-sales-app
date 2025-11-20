6b74045d8c439943c6da84e65b5c6448950cb154
yokoyamamisaki0127-cyber <yokoyamamisaki0127@gmail.com>
2025-11-21 00:36:50 +0900

    Fix router Order.vue import

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
-import Order from '../pages/Order.vue'
 import Cart from '../pages/cart.vue'
 import Checkout from '../pages/Checkout.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
-  { path: '/order', name: 'Order', component: Order },
   { path: '/cart', name: 'Cart', component: Cart },
   { path: '/checkout', name: 'Checkout', component: Checkout },
 ]
